bc81c4073b33b598678f4067637c28407ebcd76b
aashutosh-k <91001042+aashutosh-k@users.noreply.github.com>
2021-10-31 22:54:55 +0545

    added .py prog. func. def

---
+++ Projects/function_def.py
@@ -0,0 +1,15 @@
+def f(x):
+    r=3*x+5
+    return r
+
+y=f(3)
+print(y)
+y10=f(10)
+print(y10)
+###############################
+def greet(name): 
+    print("Welcome" +name)
+greet("abc") 
+greet("xyz")
+###############################
+#1.has argument ,no return value
